b7557e276ced2f30a52f2b1fa3c6cf2bf0617f4f
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-15 01:27:33 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@
 source "https://rubygems.org"
 
 # gem "rails"
+gem 'rails', '~> 5.2'
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+   if array.length == 1
+     array.join
+   elsif array.length == 2
+     array.join(" and ")
+   else
+
+     newArray = []
+
+     i = 0
+     while i < array.length - 1
+       newArray.push(array[i])
+       i += 1
+     end
+
+     i = array.length - 1
+     newArray.push("and #{array[i]}")
+     newArray.join(", ")
+   end
 
 end
